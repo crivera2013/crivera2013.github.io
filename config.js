6369660972fd86beab289d266af9cbdcbd458566
Christian Rivera <crivera43>
2020-07-31 16:21:03 -0400

    had to fix an error

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Christian Rivera',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: true,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Portfolio Optimization Dashboard',
+      path: 'https://asset-dashboard-cr.appspot.com/'
+    }
+  ],
+  author: {
+    name: 'Christian Rivera',
+    photo: '/photo.jpg',
+    bio: 'Data scientist and software developer that dabbles in that finance thing.',
+    contacts: {
+      email: 'crivera@post.harvard.edu',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'https://github.com/crivera2013',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'https://www.linkedin.com/in/christian-rivera-vanguard/',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
---
+++ content/pages/about.md
@@ -0,0 +1,9 @@
+---
+title: "About Me"
+template: "page"
+socialImage: "/media/image-2.jpg"
+---
+
+A Philadelphia techie who can't believe he is making a blog.  Currently pursuing a Masters in Computer Science at Georgia Tech.  The day job is teaching Python and predictive analytic skills to finance folks and developing software.  Also I probably watch way too much Simpsons, but that's a personal problem
+
+
